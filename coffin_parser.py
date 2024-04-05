defaf91984c11e750d5c87a3f2632809ee958792
Null_42 <zhuxinhao00@gmail.com>
2024-04-05 21:13:17 +0800

    Support printing categories

--- coffin_parser.py
+++ coffin_parser.py
@@ -1,11 +1,12 @@
 import csv
 
+CATEGORIES = ["Beast", "Demon", "Undead", "Humanoid", "Construct"]
 METADATA_FILE = "mods.csv"
 
 class CoffinParser:
     def __init__(self):
         pass
-
+    
     def load_metadata(self):
         data = {}
 
@@ -14,42 +15,40 @@ class CoffinParser:
             next(reader)
 
             for row in reader:
-                mod, cat, name = row
+                mod, mod_type, name = row
 
                 mod = mod.strip()
-                cat = cat.strip()
+                mod_type = mod_type.strip()
                 name = name.strip()
 
-                if not len(cat):
+                if not len(mod_type):
                     continue
                 
-                if cat not in data:
-                    data[cat] = {}
+                if mod_type not in data:
+                    data[mod_type] = {}
                     
-                data[cat][mod] = {"name": name, "count": 0}
-    
+                data[mod_type][mod] = {"name": name, "total_count": 0, "sub_counts": {cat: 0 for cat in CATEGORIES}}
+
         return data
     
     def parse_coffin(self, coffins, *, verbose=False):
         data = self.load_metadata()
-
-        coffin_counter = dict()
+        unparsed_mods = set()
 
         for c in coffins:
             imp = c["implicit"]
-            if imp in coffin_counter:
-                coffin_counter[imp] += 1
-            else:
-                coffin_counter[imp] = 1
+            cat = c["category"]
 
-        for mod, count in coffin_counter.items():
             success = False
-            for cat, mods in data.items():
-                if mod in mods:
-                    mods[mod]["count"] = count
+            for mode_type in data.keys():
+                if imp in data[mode_type]:
+                    data[mode_type][imp]["sub_counts"][cat] += 1
+                    data[mode_type][imp]["total_count"] += 1
                     success = True
-    
-            if verbose and not success:
-                print(f"Unknown mod: {mod}")
-        
+                    break
+            
+            if verbose and not success and imp not in unparsed_mods:
+                unparsed_mods.add(imp)
+                print("Unparsed mod:", imp)
+
         return data
--- main.py
+++ main.py
@@ -24,20 +24,21 @@ def main():
     for cat in data:
         if not len(cat):
             continue
+
         should_print = False
         for mod in data[cat]:
-            if data[cat][mod]["count"] > 0:
+            if data[cat][mod]["total_count"] > 0:
 
                 if not should_print:
                     should_print = True
-                    lines.append(f"============= {cat:^14} =============")
+                    lines.append("="*32 + f" {cat:^26} " + "="*32)
 
                 name = data[cat][mod]['name']
-                stock = data[cat][mod]['count']
+                stock = data[cat][mod]['total_count']
                 price = round(price_parser.get_price(mod))
                 
                 lines.append(
-                    f"{name:<18} | {price:<3} c/ea | Stock: {stock:<2}"
+                    f"{name:<18} | {price:<3} c/ea | Stock: {stock:<3} => Dem: {data[cat][mod]['sub_counts']['Demon']:<2} | Bea: {data[cat][mod]['sub_counts']['Beast']:<2} | Und: {data[cat][mod]['sub_counts']['Undead']:<2} | Hum: {data[cat][mod]['sub_counts']['Humanoid']:<2} | Con: {data[cat][mod]['sub_counts']['Construct']:<2}"
                 )
     
     lines = "\n".join(lines)
